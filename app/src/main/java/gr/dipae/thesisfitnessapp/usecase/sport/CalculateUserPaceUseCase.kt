44f5415f6c57777939c0b2f31587262d58904de6
ptoumpas <ptoumpas@arx.net>
2023-08-04 15:33:07 +0300

    Fix pace not upper limit

--- app/src/main/java/gr/dipae/thesisfitnessapp/usecase/sport/CalculateUserPaceUseCase.kt
+++ app/src/main/java/gr/dipae/thesisfitnessapp/usecase/sport/CalculateUserPaceUseCase.kt
@@ -5,6 +5,6 @@ import javax.inject.Inject
 class CalculateUserPaceUseCase @Inject constructor() {
     operator fun invoke(timeTravellingInMillis: Long, distanceTravelledInKm: Double): Long {
         val minutesTravelling = ((timeTravellingInMillis / (1000 * 60f)).mod(60f))
-        return (minutesTravelling / distanceTravelledInKm).toLong()
+        return (minutesTravelling / distanceTravelledInKm).toLong().coerceAtMost(120)
     }
 }
